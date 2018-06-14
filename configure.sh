13a29d69a767e21f94a1004c490b1b6edef852cb
Jonathan YUE CHUN <jonathan.yue-chun@sncf.fr>
2018-06-14 21:52:44 +0000

    test

--- configure.sh
+++ configure.sh
@@ -4,7 +4,7 @@ npm install typescript@'>=2.1.0 <2.4.0'
 npm install http
 npm install http-auth
 cp node_modules/save-svg-as-png/lib/* node_modules/save-svg-as-png/
-#ng build --prod --build-optimizer --sourcemaps
-#mkdir app
-#mv dist app/
+ng build --prod --build-optimizer --sourcemaps
+mkdir app
+mv dist app/
 echo "Configuration ended successfully !"
--- server.js
+++ server.js
@@ -19,6 +19,3 @@ app.get('/*', function(req,res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8086);
-
-// Authentication module.
-
