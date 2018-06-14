2cf2315a02dbd121a4e3f2a1b1356f0f1b2871c5
Jonathan YUE CHUN <jonathan.yue-chun@sncf.fr>
2018-06-14 20:02:12 +0000

    fixeddd

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const path = require('path');
 
 const app = express();
 
-var basicAuth = require('basic-auth-connect');
-app.use(basicAuth('username', 'password'));
-
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/app/dist'));
 
