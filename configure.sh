f82b23baf29b1f5aa7f7b2c0baeae01a6319d29e
Jonathan YUE CHUN <jonathan.yue-chun@sncf.fr>
2018-06-14 18:52:20 +0000

    fix error

--- configure.sh
+++ configure.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Starting configuration..."
-npm install
 cp node_modules/save-svg-as-png/lib/* node_modules/save-svg-as-png/
 ng build --prod --build-optimizer --sourcemaps
 mkdir app
