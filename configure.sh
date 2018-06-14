241c8a8b79cdaa76fccfe545bcf0bd62903ae5d4
Jonathan YUE CHUN <jonathan.yue-chun@sncf.fr>
2018-06-14 21:28:10 +0000

    ok

--- configure.sh
+++ configure.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Starting configuration..."
 npm install typescript@'>=2.1.0 <2.4.0'
+npm install http-auth
 cp node_modules/save-svg-as-png/lib/* node_modules/save-svg-as-png/
-ng build --prod --build-optimizer --sourcemaps
-mkdir app
-mv dist app/
+#ng build --prod --build-optimizer --sourcemaps
+#mkdir app
+#mv dist app/
 echo "Configuration ended successfully !"
--- server.js
+++ server.js
@@ -1,16 +1,32 @@
-//Install express server
+// //Install express server
 const express = require('express');
 const path = require('path');
 
-const app = express();
+// const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/app/dist'));
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/app/dist'));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/app/dist/index.html'));
-});
+// res.sendFile(path.join(__dirname+'/app/dist/index.html'));
+// });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8086);
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8086);
+
+// Authentication module.
+var auth = require('http-auth');
+var basic = auth.basic({
+    realm: "Simon Area.",
+    file: __dirname + "/../data/users.htpasswd"
+});
+ 
+// Application setup.
+const app = express();
+app.use(auth.connect(basic));
+ 
+// Setup route.
+app.get('/', (req, res) => {
+    res.send(`Hello from express - ${req.user}!`);
+});
