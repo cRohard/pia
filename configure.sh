ea1043d39940cb2921625ffefc9c592b97af469e
Jonathan YUE CHUN <jonathan.yue-chun@sncf.fr>
2018-06-14 19:29:53 +0000

    fixeddd

--- configure.sh
+++ configure.sh
@@ -2,6 +2,7 @@
 echo "Starting configuration..."
 npm install typescript@'>=2.1.0 <2.4.0'
 cp node_modules/save-svg-as-png/lib/* node_modules/save-svg-as-png/
+ng build --prod --build-optimizer --sourcemaps
 mkdir app
-
+mv dist app/
 echo "Configuration ended successfully !"
